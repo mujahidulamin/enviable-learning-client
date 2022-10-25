5b2d4d4de938be24ab77ecf1d42b2b982cc7e5c1
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2022-10-25 18:31:42 +0600

    react-to-pdf-execute-done

--- src/components/CourseDetails/CourseDetails.js
+++ src/components/CourseDetails/CourseDetails.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
+import Pdf from "react-to-pdf";
+import { FaFilePdf } from 'react-icons/fa';
+
+const ref = React.createRef();
+
+
 
 const CourseDetails = () => {
+
+
+
     const courseDetails = useLoaderData()
     console.log(courseDetails);
 
     const { course_title, image, instructor_name, price, description, instructor_photo } = courseDetails
 
+
     return (
         <div className='px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
-            <div>
+            <div >
                 <div className='bg-gray-100 p-6 rounded shadow-lg'>
-                    <h2 className='text-3xl font-bold text-center mb-5'>{course_title}</h2>
+                    <h2 className='text-3xl font-bold text-center mb-5'>{course_title} <Pdf targetRef={ref} filename="description.pdf">
+                        {({ toPdf }) => <button onClick={toPdf}><FaFilePdf></FaFilePdf></button>}
+                    </Pdf></h2>
+
                     <img
                         className='object-cover w-full mb-6 rounded shadow-lg xl:h-80'
                         src={image}
                         alt=''
                     />
-                    <div className='text-justify'>
+                    <div ref={ref} className='text-justify'>
                         <span className='font-bold '>Description: </span>
                         {description}
                     </div>
@@ -27,7 +40,7 @@ const CourseDetails = () => {
                         <div>
                             <div className='border flex bg-slate-400 w-48'>
                                 <img className='h-12 w-12 rounded-full' src={instructor_photo} alt="" />
-                                <span className='ml-2 mt-3'>{instructor_name}</span>
+                                <span className='ml-2 mt-3 font-bold'>{instructor_name}</span>
                             </div>
                         </div>
 
@@ -36,6 +49,9 @@ const CourseDetails = () => {
                         </div>
                     </div>
 
+                    <Link>
+                        <button className='bg-amber-300 p-3 rounded-xl flex m-auto mt-6'>Get Premium Access</button>
+                    </Link>
 
                 </div>
             </div>
